b2346b8ecda512b324e6b4b3b5753e8f2d243884
Timothy Recker <timjrecker@gmail.com>
2024-05-15 19:15:10 -0400

    template for (planned) next level of metalinguistic abstraction

---
+++ mccarthy/lisp.lisp.lisp
@@ -0,0 +1,14 @@
+;;; File: lisp.lisp.lisp
+;;; Description: This program uses LISP to implement LISP2 (pronounced "LISP squared").
+
+(cl:defpackage :lisp2
+  (:export
+   :eval)
+  (:import-from
+   :lisp
+   :t :nil)
+  )
+
+(cl:in-package "LISP2")
+
+;; TODO: implement
